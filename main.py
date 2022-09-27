1afc2404cc6087997689516d1101c0aa8d8eedef
root <root@dev.asia-southeast1-a.c.metal-hope-359816.internal>
2022-09-27 08:08:36 +0000

    edit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request , render_template
 import shapefile
 from flask_cors import CORS
-import json,os
+import json,os,zipfile
+import patoolib
 from shapefile_reader import *
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def hello():
 
 @app.route('/api/data')
 def hello_world():
-    response = getLatLongFromShapeFile("shapefile/"+request.args['file'])
+    response = getLatLongFromShapeFile("shapefile/"+os.path.splitext(request.args['file'])[0]+"/"+request.args['file'])
     return response
 
 @app.route('/api/data', methods=['POST'])
@@ -21,16 +22,43 @@ def uploadShp():
     # return "hello"
     record1 = request.files['shp']
     record2 = request.files['dbf']
-    print(record1)
+    record3 = request.files['shx']
+    record4 = request.files['prj']
+
+    os.makedirs("shapefile/"+os.path.splitext(record1.filename)[0], exist_ok=True)
     # record.save(secure_filename(record.filename))
-    fn1 = "shapefile/"+record1.filename
+    fn1 = "shapefile/"+os.path.splitext(record1.filename)[0]+"/"+record1.filename
     open(fn1, 'wb').write(record1.read())
 
-    fn2 = "shapefile/"+record2.filename
+    fn2 = "shapefile/"+os.path.splitext(record1.filename)[0]+"/"+record2.filename
     open(fn2, 'wb').write(record2.read())
+
+    fn3 = "shapefile/"+os.path.splitext(record1.filename)[0]+"/"+record3.filename
+    open(fn3, 'wb').write(record3.read())
+     
+    fn4 = "shapefile/"+os.path.splitext(record1.filename)[0]+"/"+record4.filename
+    open(fn4, 'wb').write(record4.read())
     # print(record)
     # response = getLatLongFromShapeFile("shapefile/"+record1.filename)
     return record1.filename
+
+@app.route('/api/data/zipfile', methods=['POST'])
+def uploadZip():
+    # return "hello"
+    record1 = request.files['zip']
+
+    # os.makedirs("zip/"+os.path.splitext(record1.filename)[0], exist_ok=True)
+    # record.save(secure_filename(record.filename))
+    fn = "zip/"+record1.filename
+    open(fn, 'wb').write(record1.read())
+
+    # patoolib.extract_archive("zip/"+record1.filename, "shapefile/"+os.path.splitext(record1.filename)[0]+"/"+os.path.splitext(record1.filename)[0]+".shp")
+
+    with zipfile.ZipFile("zip/"+record1.filename,"r") as zip_ref:
+        zip_ref.extractall("shapefile/"+os.path.splitext(record1.filename)[0]+"/"+os.path.splitext(record1.filename)[0]+".shp")
+    # print(record)
+    # response = getLatLongFromShapeFile("shapefile/"+record1.filename)
+    return os.path.splitext(record1.filename)[0]+".shp"
 
 # main driver function
 if __name__ == '__main__':
--- shapefile_reader.py
+++ shapefile_reader.py
@@ -1,26 +1,46 @@
+import string
 import shapefile
 import pandas as pd
+import json
+import geopandas,pandas
 
-shp_file = "./shapefile/979fc2b6b36d3aae-polygon.shp"
+shp_file = "./shapefile/other/979fc2b6b36d3aae-polygon.shp"
 
 def swapCoordinate(coordinates) :
-    coordinates[0] = [list(s) for s in coordinates[0]]
+    coordinates = [list(s) for s in coordinates]
 
     long = [s[0] for s in coordinates[0]]
     lat = [s[1] for s in coordinates[0]]
     df = pd.DataFrame(list(zip(lat,long)))
 
     new_coordinate = df.values.tolist()
+
     return new_coordinate
 
-
 def getLatLongFromShapeFile(shp_file) :
-    geojson_data = shapefile.Reader(shp_file, encoding = 'unicode_escape').__geo_interface__
-
+    geojson_data = geopandas.read_file(shp_file)
+    old_crs = geojson_data.crs
+    data = geojson_data.to_crs(epsg = 4326)
     id = 0
-    coordinates = geojson_data["features"][id]["geometry"]["coordinates"]
+    datas = data
+    datas.__geo_interface__["features"][id]["geometry"]["coordinates"] = swapCoordinate(data.__geo_interface__["features"][id]["geometry"]["coordinates"])
     
-    geojson_data["features"][id]["geometry"]["coordinates"] = swapCoordinate(coordinates)
-    return geojson_data
+    response = {
+        "old_crs" : "{}".format(old_crs),
+        "new_crs" : "epsg:4326",
+        "features": [
+            {
+                "bbox" : datas.__geo_interface__["features"][id]["bbox"][::-1],
+                "geometry" : {
+                    "coordinates": swapCoordinate(data.__geo_interface__["features"][id]["geometry"]["coordinates"]),
+                    "type" : datas.__geo_interface__["features"][id]["geometry"]["type"]
+                },
+                "id": "0",
+                "properties": datas.__geo_interface__["features"][id]["properties"],
+                "type": datas.__geo_interface__["features"][id]["type"]
+            }
+        ],
+        "type": datas.__geo_interface__["type"]
+    }
 
-# getLatLongFromShapeFile()
+    return json.dumps(response)
